d89b4d85e9791acf2a32748e245fa336dfba4f8c
Jonathan Kaufman <jondakauf@gmail.com>
2016-05-17 15:01:47 -0400

    initial commit - variation of socket.io starter chat app without jquery

---
+++ server.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const bodyParser = require('body-parser');
+
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.text());
+
+app.get('/', function(req, res){
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(app.get('port'), () => {
+  console.log(`listening on ${app.get('port')}`);
+});
